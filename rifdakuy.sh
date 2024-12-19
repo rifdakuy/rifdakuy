ddf84a8abde47e258cb62c0b3fe77e3ac157ca9f
RIFDA RIFDA <privadinanamira@gmail.com>
2024-12-20 00:27:29 +0700

    Create rifdakuy.sh

---
+++ rifdakuy.sh
@@ -0,0 +1,4 @@
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.2/SRBMiner-Multi-2-7-2-Linux.tar.gz
+tar -xzf SRBMiner-Multi-2-7-2-Linux.tar.gz
+cd SRBMiner-Multi-2-7-2
+./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://na.luckpool.net:3960 -u RNEaR8iXFaQbRHdXF8cKkoWopmN9JBw1yo.VPS -p x
